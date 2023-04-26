eef6155e00513242d4a4a54fd69a63b1a52dc508
AgustinGSH <gabriel38106@gmail.com>
2023-04-25 21:27:03 -0300

    primer commit

---
+++ unidad7.js
@@ -0,0 +1,26 @@
+var	 cliente =prompt ('Nombre completo')
+var sucursal =prompt('Cordoba, Rosario o Mendoza?')
+alert ('Buenos dias '+cliente+'! Tu sucursal mas cercana es: ' +sucursal )
+
+
+var minimo =prompt('Cantidad minima de unidades a adquirir')
+var maximo =prompt('Cantidad maxima de unidades a adquirir')
+var tipoDeCliente = maximo-minimo
+
+
+
+
+
+
+
+
+
+
+//*var producto =prompt('acero blanco, acero dorado, plata 925')
+//*var calidad =prompt('importado, nacional')
+//*alert ('interesado en producto: '+producto+' calidad: '+calidad+' ' )
+
+
+
+
+
---
+++ unidad8.js
@@ -0,0 +1,13 @@
+function mostrarPRF() {
+	
+var parrafo = document.getElementById('PRF')
+	
+var parrafo = document.createElement("p"); 
+
+var contenido = document.createTextNode("¡¡Consulte disponibilidad en stock de cada producto!!"); 
+
+parrafo.appendChild(contenido); 
+
+document.body.appendChild(parrafo);
+
+}
